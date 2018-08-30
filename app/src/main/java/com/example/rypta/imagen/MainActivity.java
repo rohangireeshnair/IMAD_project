f1c603a58ce25f1300e1404483bc3fa7835d8b65
Rohan G Nair <rohangireeshnair@gmail.com>
2018-08-30 16:26:15 +0530

    second commit -rgn

--- app/src/main/java/com/example/rypta/imagen/MainActivity.java
+++ app/src/main/java/com/example/rypta/imagen/MainActivity.java
@@ -2,12 +2,22 @@ package com.example.rypta.imagen;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow(). requestFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
+
+        TextView uname, passwd;
+        Button signup, login;
+        
+
+
     }
 }
